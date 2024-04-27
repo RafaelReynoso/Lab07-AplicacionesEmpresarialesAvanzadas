2d027caf4acfcd657840d2e371a3f2a10c195834
Alumno <Alumno@LAB1504-28>
2024-04-27 12:53:45 -0500

    Agregar archivos de proyecto.

---
+++ Business/CustomerBusiness.cs
@@ -0,0 +1,22 @@
+﻿using Data;
+using Entity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business
+{
+    public class CustomerBusiness
+    {
+        public List<Customer> GetCustomer()
+        {
+            List<Customer> customer = new List<Customer>();
+            CustomerData data = new CustomerData();
+            customer = data.GetCustomers();
+            return customer;
+
+        }
+    }
+}
---
+++ Data/CustomerData.cs
@@ -0,0 +1,56 @@
+﻿using Entity;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace Data
+{
+    public class CustomerData
+    {
+        public string connectionString = "Data Source=LAB1504-28\\SQLEXPRESS;Initial Catalog=lab07;User Id=reynoso;Password=123";
+        public List<Customer> GetCustomers()
+        {
+            List<Customer> customers = new List<Customer>();
+
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                SqlCommand command = new SqlCommand("USP_GetAllCustomers", connection);
+                command.CommandType = CommandType.StoredProcedure;
+                connection.Open();
+
+                using (SqlDataReader reader = command.ExecuteReader())
+                {
+                    while (reader.Read())
+                    {
+                        Customer customer = new Customer();
+                        customer.customer_id = Convert.ToInt32(reader["customer_id"]);
+                        customer.name = reader["name"].ToString();
+                        customer.address = reader["address"].ToString();
+                        customer.phone = reader["phone"].ToString();
+                        customer.active = Convert.ToBoolean(reader["active"]);
+
+                        customers.Add(customer);
+                    }
+                }
+            }
+
+            return customers;
+        }
+
+        public void Insert()
+        {
+            
+        }
+
+        public void Update()
+        {
+            
+        }
+
+        public void Delete()
+        {
+            
+        }
+    }
+}
